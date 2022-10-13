64d253393988c68e4fdaa8faaa5c2b6bcc5e75f4
E-OssO <damien.percereau@gmail.com>
2022-10-13 14:35:08 +0000

    Deploying to gh-pages from master @ b1a16d36165899f7b6df9bc1f01ff779a6af2065 🚀

---
+++ assets/main.0330f8ae.js
@@ -0,0 +1,1 @@
+WA.onInit().then(async()=>{WA.controls&&WA.controls.disablePlayerProximityMeeting&&(console.log("disablePlayerProximityMeeting"),WA.controls.disablePlayerProximityMeeting())}).catch(e=>console.error(e));
